1575a1e32758371b7c4e37f058384dcf03ef0f5a
wasiqah Neyaz <wasiqahneyaz144@gmail.com>
2024-08-31 20:42:44 +0530

    com

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div>
+      
+     
+      <div class="custom-loader">
+      
+      </div>
+      
+    </div>
+  )
+}
+
+export default Spinner
